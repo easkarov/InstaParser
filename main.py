47dbee2181c2bb6bd6bad89e60e60b2ea6c34961
Emil <askarov.emii@yandex.ru>
2021-07-27 15:03:46 +0500

    minor corrections

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 from time import sleep
-from typing import List, Optional, Dict
+from typing import List, Optional, Dict, Tuple
 import random
-# from multiprocessing.pool import ThreadPool
+from multiprocessing.pool import ThreadPool
 
 import pickle
+import uuid
 
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.common.exceptions import NoSuchElementException
@@ -34,9 +35,9 @@ class InstaParser:
 
         self._browser.get('https://www.instagram.com/')
 
-        if self._load_cookies():
-            sleep(5)
-            return
+        # if self._load_cookies():
+        #     sleep(5)
+        #     return
 
         logger.info('логинимся...')
 
@@ -50,6 +51,8 @@ class InstaParser:
 
         password_input.send_keys(Keys.ENTER)
 
+        logger.info('авторизация прошла успешно')
+
         sleep(5)
 
     def search_posts_by_hashtag(self, hashtag: str, n: int = 100) -> List[str]:
@@ -57,7 +60,7 @@ class InstaParser:
 
         self._browser.get(f'https://www.instagram.com/explore/tags/{hashtag}/')
         logger.info('загружена страница с лентой постов по хэштегу')
-        sleep(7)
+        sleep(15)
 
         posts_urls = [link.get_attribute('href')
                       for link in self._browser.find_elements_by_tag_name('a')
@@ -77,10 +80,11 @@ class InstaParser:
 
         return posts_urls[num_of_top_posts:]
 
-    def filter_short_posts(self, urls: List[str], min_symbols: int = 100) -> List[str]:
+    def filter_short_posts(self, urls: List[str], min_symbols: int = 100) -> Tuple[List[str], List[str]]:
         """This method filters out short posts, whose length is less than a certain number (min_symbols)"""
 
         long_posts_urls = []
+        long_posts_text = []
         for url in urls:
             description = self.get_post_description(url)
 
@@ -88,11 +92,12 @@ class InstaParser:
                 continue
 
             long_posts_urls.append(url)
+            long_posts_text.append(description)
             logger.info(f'найден длинный пост: {url}')
 
             sleep(random.randint(2, 4))
 
-        return long_posts_urls
+        return long_posts_urls, long_posts_text
 
     def get_post_description(self, url: str) -> Optional[str]:
         try:
@@ -105,15 +110,15 @@ class InstaParser:
         except NoSuchElementException:
             return None
 
-    def get_long_posts_by_hashtag(self, hashtag: str, num_of_posts: int = 100) -> List[str]:
+    def get_long_posts_by_hashtag(self, hashtag: str, num_of_posts: int = 100) -> Tuple[List[str], List[str]]:
         """This method return a ready list of links of long posts"""
 
         posts_urls = self.search_posts_by_hashtag(hashtag, n=num_of_posts)
         logger.info(f'собрано всего ссылок на посты: {len(posts_urls)}')
-        filtered_posts = self.filter_short_posts(posts_urls)
-        logger.info(f'посты отфильтрованы. Кол-во длинных постов: {len(filtered_posts)}')
+        filtered_post_urls, filtered_post_text = self.filter_short_posts(posts_urls)
+        logger.info(f'посты отфильтрованы. Кол-во длинных постов: {len(filtered_post_urls)}')
 
-        return filtered_posts
+        return filtered_post_urls, filtered_post_text
 
     def check_if_xpath_exists(self, xpath: str) -> Optional[WebElement]:
         """Checking that the element xpath exists"""
@@ -191,21 +196,28 @@ class InstaParser:
     def run(cls, hashtag):
         examp = cls(username=LOGIN, password=PASSWORD)
         examp.login()
-        posts = examp.get_long_posts_by_hashtag(hashtag, num_of_posts=10)
-        filename = 'tag_' + hashtag
-        with open('./posts/' + filename, 'w') as file:
-            file.write('\n'.join(posts))
+        post_urls, post_text = examp.get_long_posts_by_hashtag(hashtag, num_of_posts=10)
+        filename = 'tag_' + hashtag + '.txt'
+        path = f'./posts/{hashtag}/'
+
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        with open(path + filename, 'a') as file:
+            file.write('\n'.join(post_urls))
+
+        for text in post_text:
+            filename = uuid.uuid4().hex + '.txt'
+            with open(path + filename, 'w', encoding='utf-16') as file:
+                file.write(text)
+
+        logger.info('все найденные ссылки записаны в файлы')
+
         examp.close_browser()
 
 
 def main():
-    # hashtags = ['полезныесоветы', 'ремонт']
-    #
-    # pool = ThreadPool(processes=2)
-    # pool.map(InstaParser.run, hashtags)
-    hashtag = 'полезныесоветы'
-
-    parser = InstaParser(username=LOGIN, password=PASSWORD)
+    # parser = InstaParser(username=LOGIN, password=PASSWORD)
     # if not parser.load_cookies():
     #     parser.login()
     # parser.go_to('https://instagram.com')
@@ -216,18 +228,19 @@ def main():
     #         parser.load_cookies(cookies)
     #         logger.info('куки успешно загрузились')
     # else:
-    parser.login()
-    logger.info('авторизация прошла успешно')
 
-    links = parser.get_long_posts_by_hashtag(hashtag, num_of_posts=50)
-    filename = 'tag_' + hashtag + '.txt'
-    with open('./posts/' + filename, 'w') as file:
-        file.write('\n'.join(links))
-        logger.info('все найденные ссылки записаны в файлы')
+    hashtags = ['полезныесоветы']
+
+    pool = ThreadPool(processes=1)
+    pool.map(InstaParser.run, hashtags)
+    # links = parser.get_long_posts_by_hashtag(hashtag, num_of_posts=50)
+    # filename = 'tag_' + hashtag + '.txt'
+    # with open('./posts/' + filename, 'w') as file:
+    #     file.write('\n'.join(links))
 
     # parser.dump_cookies()
 
-    parser.close_browser()
+
 
 if __name__ == '__main__':
     main()
